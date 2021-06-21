69b70023aaab960460ea3ec5646f7c8155ab2c06
jeffseverson2001 <jeff@zealouscreations.com>
2021-06-21 18:00:02 -0500

    Monday Edit

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,11 +4,11 @@ describe("Employee class", () => {
     //  Test for a cases when initializing a new Employee object
     describe("Initialization", () => {
         it("should create an object with name, id, email and github name if valid arguments", () => {
-            const employee = new Employee("Test Employee", 1000, "testemployee@email.com");
+            const employee = new Employee(1000, "Test Employee", "testemployee@email.com");
 
             //  Verifiy new oject has been created
-            expect(employee.name).toEqual("Test Employee");
             expect(employee.id).toEqual(1000);
+            expect(employee.name).toEqual("Test Employee");
             expect(employee.email).toEqual("testemployee@email.com");
         });
 
--- index.js
+++ index.js
@@ -4,16 +4,40 @@ const fs = require("fs");
 const Employee = require("./lib/Employee");
 
 
-//  Add imput questions.
+//  Add input questions.
+let questions = [
+    {
+        type: "input",
+        message: "Enter Employee Name",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "Enter Employee Email",
+        name: ""
+    },
+    {
+        type: "input",
+        message: "",
+        name: ""
+    }
+];
 
 
 //  Add team member to list
-
+function init(role) {
+    inquirer.prompt(questions)
+    .then((data) => {
+        console.log(data)
+        //writeToFile("./output/README.md", data)
+    })
+}
 
 //  Build HTML page
 
 
 //  Initate team manager info
+init("manager");
 
 
 
